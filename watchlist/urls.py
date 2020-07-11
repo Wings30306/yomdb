83d5076c5ecdd9799b50b1d77b537219fc026478
Joke Heyndels <jo_hannah@outlook.com>
2020-07-11 16:38:58 +0200

    Comment out changes to watchlist views and urls

--- watchlist/urls.py
+++ watchlist/urls.py
@@ -1,8 +1,7 @@
 
 from django.urls import path, reverse_lazy
-from .views import WatchList
+# from .views import WatchList
 
 app_name = "watchlist"
 urlpatterns = [
-    path("", watchlist_view, name="watchlist")
 ]
--- watchlist/views.py
+++ watchlist/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Watchlist_Item
+# from .models import Watchlist_Item
 
-# Create your views here.
-class WatchList(ListView):
-    template_name="watchlist.html"
-    queryset = Watchlist_Item.objects.all()
+# # Create your views here.
+# class WatchList(ListView):
+#     template_name="watchlist.html"
+#     queryset = Watchlist_Item.objects.all()
