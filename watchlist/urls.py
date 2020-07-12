ce9ce3fd1afea8b0e0b597958cf4df62d410cb9c
Joke Heyndels <jo_hannah@outlook.com>
2020-07-12 16:59:32 +0200

    Watchlist: create templates, views and urls

--- watchlist/urls.py
+++ watchlist/urls.py
@@ -1,8 +1,11 @@
 
 from django.urls import path, reverse_lazy
-from .views import Watchlist
+from .views import Watchlist, watchlist_detail, CreateWatchlistItem, add_movie
 
 app_name = "watchlist"
 urlpatterns = [
-    path("", Watchlist, name="watchlist")
+    path("", Watchlist, name="watchlist"),
+    path("<int:primary_key>/", watchlist_detail, name="watchlist_detail"),
+    path("new/", CreateWatchlistItem.as_view(), name="create_watchlist_item"),
+    path("add-movie/", add_movie, name="add_movie"),
 ]
--- watchlist/views.py
+++ watchlist/views.py
@@ -1,19 +1,49 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect, reverse
+from django.views.generic import CreateView
 from django.conf import settings
 import requests
 import json
-# from .models import Watchlist_Item
-
-def loadApi():
-    print(settings.API)
-    response = json.loads(requests.get(settings.API).text)
-    print(response)
-
+from datetime import date
+from .models import WatchlistItem, Movie
 
 def Watchlist(request):
-    return render(request, "watchlist.html")
+    context = {
+        "queryset": WatchlistItem.objects.filter(user=request.user)
+    }
+    template_name = "watchlist.html"
+    return render(request, template_name, context)
 
+def watchlist_detail(request, primary_key):
+    """Shows details for a specific service"""
+    template_name = 'watchlist-detail.html'
+    if request.method == "POST":
+        obj = WatchlistItem.objects.get(pk=primary_key)
+        obj.watched = not obj.watched
+        obj.save()
+        return redirect('watchlist:watchlist_detail', primary_key=primary_key)
+    try:
+        obj = WatchlistItem.objects.get(pk=primary_key)
+        context = {
+            "object": obj
+        }
+    except WatchlistItem.DoesNotExist:
+        messages.error(request, 'No movie in watchlist with this id: ' +
+                       primary_key + '</em>.')
+        return redirect("watchlist:watchlist")
+    return render(request, template_name=template_name, context=context)
 
+class CreateWatchlistItem(CreateView):
+    model = WatchlistItem
+    fields = ["movie", "watched"]
 
-    
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.user = self.request.user
+        date_added = date.today()
+        obj.save()
+        return redirect("watchlist:watchlist_detail", primary_key= obj.pk)
+
+def add_movie(request):
+    if request.method == "post":
+        print("Add this movie to database")
+    return render(request, "new-movie.html")
