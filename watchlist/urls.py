23a636c5aca073012d5ba4b5699fbd34ea5569ab
Joke Heyndels <jo_hannah@outlook.com>
2020-07-23 20:03:18 +0200

    Add url for delete view

--- watchlist/urls.py
+++ watchlist/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path, reverse_lazy
-from .views import Watchlist, watchlist_detail, CreateWatchlistItem, add_movie
+from .views import Watchlist, watchlist_detail, CreateWatchlistItem, add_movie, delete_watchlist_item
 
 app_name = "watchlist"
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path("<int:primary_key>/", watchlist_detail, name="watchlist_detail"),
     path("new/", CreateWatchlistItem.as_view(), name="create_watchlist_item"),
     path("add-movie/", add_movie, name="add_movie"),
+    path("<int:primary_key>/delete", delete_watchlist_item, name="delete_watchlist_item")
 ]
