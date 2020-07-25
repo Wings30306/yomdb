d675610dd8994e6a910d239e3ccdbfa149d5e1ad
Joke Heyndels <jo_hannah@outlook.com>
2020-07-25 20:44:39 +0200

    Write tests for Accounts app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class AccountsTests(TestCase):
+
+    def test_homepage(self):
+        """ Test that homepage is rendered """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page(self):
+        """ Check that page redirects to secure site (https) """
+        response = self.client.get('/accounts/login')
+        self.assertEqual(response.status_code, 301)
+
+    def test_logout(self):
+        """ Test that user leaves secure zone on logging out """
+        response = self.client.get('/accounts/logout')
+        self.assertEqual(response.status_code, 301)
+    
+class UserModelTest(TestCase):
+    def test_string_representation(self):
+        user = User(username="TestUser")
+        self.assertEqual(str(user), user.username)
