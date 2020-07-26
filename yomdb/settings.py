56da8ceb3f68ff2ead3fb84c9cd7aabc84dd884b
Joke Heyndels <jo_hannah@outlook.com>
2020-07-26 12:30:26 +0200

    Add allowed host to settings.py

--- yomdb/settings.py
+++ yomdb/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEV")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "your-own-movie-database.herokuapp.com"]
 
 
 # Application definition
